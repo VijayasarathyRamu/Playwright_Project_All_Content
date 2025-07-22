6ab25f5b53bcc5941bff24032c10b46e41f4b07e
VijayasarathyRamu <vijayasarathy.ramu@gmail.com>
2025-07-22 18:26:16 +0530

    by vijay correction of this code 22/7/25

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,5 +1,6 @@
 import {expect, test} from '@playwright/test';
 
+// test for get request
 test("should mock get request",async({page})=>{
     await page.route("api/users",(route)=>{
         route.fulfill({
@@ -15,5 +16,40 @@ test("should mock get request",async({page})=>{
     await page.goto("http://127.0.0.1:5500/mokeapi.html");
     const userList = await page.locator(".user").allTextContents();
     await expect(userList).toContain("Vijay")
+
+});
+
+// based parameter perform grt request parameter can id or any
+
+test("get request on the rol basis",async({page})=>{
+    await page.route("api/users?role=admin",(route)=>{
+        route.fulfill({
+            status:200,
+            contentType:'application/json',
+            body:JSON.stringify([
+                {id:1,name:"Vijay"}
+            ]),
+        });
+
+    });
+    await page.goto("http://127.0.0.1:5500/mokeapi.html");
+    const userList = await page.locator(".user").allTextContents();
+    await expect(userList).toContain("Vijay")
+
+});
+
+//server error - to handle internal server error
+test("test to handle 500 error",async({page})=>{
+    await page.route("api/users",(route)=>{
+        route.fulfill({
+            status:500,
+            contentType:'application/json',
+            body:"Internal Server Error"
+        });
+
+    });
+    await page.goto("http://127.0.0.1:5500/mokeapi.html");
+    const userList = await page.locator(".useerror").allTextContents();
+    await expect(userList).toContain("Internal Server Error")
 
 });
